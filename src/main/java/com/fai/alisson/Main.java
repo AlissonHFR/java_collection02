54f96487b9b05bb595a4f4d5e421f3191bb94a27
Alisson <alissonhenrique98@gmail.com>
2020-03-11 19:43:00 -0300

    primmeiro commit

---
+++ src/main/java/com/fai/alisson/Main.java
@@ -0,0 +1,17 @@
+package com.fai.alisson;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+		
+
+	}
+
+	private void start() {
+		
+		
+	}
+
+}
