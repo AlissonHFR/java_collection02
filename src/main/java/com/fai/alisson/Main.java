2e8f09c823f785e72199601ff7326bc32445d19b
Alisson <alissonhenrique98@gmail.com>
2020-03-11 19:55:44 -0300

    commit com parte inicial do mapa

--- src/main/java/com/fai/alisson/Main.java
+++ src/main/java/com/fai/alisson/Main.java
@@ -1,5 +1,8 @@
 package com.fai.alisson;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -8,10 +11,27 @@ public class Main {
 		
 
 	}
+	
+	private Map<String, String> mapa = new HashMap<String, String>();
 
 	private void start() {
-		
+		adicionarItensNoMapa();
+		exibirValorDoMapaPelaChave("PA");
 		
 	}
-
+	private void adicionarItensNoMapa(){
+		
+		mapa.put("SRS", "Santa Rita do Sapucaí");
+		mapa.put("PA", "Pouso Alegre");
+	}
+	private void exibirValorDoMapaPelaChave(String chave) {
+		String valorDoItemDoMapa = mapa.get(chave);
+		if(valorDoItemDoMapa ==null || valorDoItemDoMapa.isEmpty()) {
+			System.out.println("Esta chave não está presente nesse mapa " + chave);
+		}else {
+			System.out.println("O valor dessa chave é: " + mapa.get(chave));
+		}
+			
+		
+	}
 }
